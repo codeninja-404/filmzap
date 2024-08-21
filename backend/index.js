08f8757ca376a11f996400cd783341358d0d6ea6
Saidul Arefin <saidularefin8@gmail.com>
2024-08-21 20:19:47 +0600

    first commit on filmzap app

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+import express from "express";
+import authRoutes from "./routes/authRoutes.js";
+const app = express();
+
+app.use("/api/v1/auth", authRoutes);
+app.listen(5000, () => {
+  console.log("Server listening on 5000");
+});
