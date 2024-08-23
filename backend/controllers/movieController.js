b7c34117e461e5305b8f078f7199697f54448998
Saidul Arefin <saidularefin8@gmail.com>
2024-08-23 21:12:02 +0600

    feat(controller): add movies by category endpoint with error handling

--- backend/controllers/movieController.js
+++ backend/controllers/movieController.js
@@ -64,4 +64,20 @@ export const getSimilarMovies = async (req, res) => {
   }
 };
 
-export const getMoviesByCategory = async (req, res) => {};
+export const getMoviesByCategory = async (req, res) => {
+  const { category } = req.params;
+  try {
+    const data = await fetchFromTMDB(
+      `https://api.themoviedb.org/3/movie/${category}?language=en-US&page=1`,
+    );
+    res.status(200).json({ success: true, content: data.results });
+  } catch (error) {
+    if (error.message.includes("404")) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Movies not found" });
+    }
+    console.error("Error in movies by category controller:", error.message);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
