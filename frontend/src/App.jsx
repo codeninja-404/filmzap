5adcc4252cf2bae0310c72f2af7b1630dac82f32
Saidul Arefin <saidularefin8@gmail.com>
2024-08-24 22:48:39 +0600

    feat: add routing for Home, Login, and SignUp pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
-import Logo from "./assets/FILMZAP.svg";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
 
 function App() {
   return (
-    <>
-      <div>
-        <a href="#" target="_blank">
-          <img src={Logo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+    </Routes>
   );
 }
 
