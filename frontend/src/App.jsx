2bfaa01393166b4d901e44a90f0ecd8284953f5e
Saidul Arefin <saidularefin8@gmail.com>
2024-08-24 22:41:16 +0600

    feat: initialize React project with Vite and install dependencies

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import Logo from "./assets/FILMZAP.svg";
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="#" target="_blank">
+          <img src={Logo} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+    </>
+  );
+}
+
+export default App;
